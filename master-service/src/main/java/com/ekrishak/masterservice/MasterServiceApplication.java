dc06987ebdad5f3bf13156205c17c2ca13a2fb66
Chhavinath Sahu <chhavinath@infominez.com>
2024-03-09 13:59:25 +0530

    added user service and master service

---
+++ master-service/src/main/java/com/ekrishak/masterservice/MasterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ekrishak.masterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterServiceApplication.class, args);
+	}
+
+}
